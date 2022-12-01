afcb2402702adb3be5a965c37ed4374432710732
Lorenzo Curzi <l.curzi@sogeitsolutions.com>
2022-12-01 19:59:40 +0100

    Day 1

---
+++ AdventOfCode2022_Csharp/AdventOfCode2022.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022_Csharp
+{
+    class AdventOfCode2022
+    {
+        static void Main(string[] args)
+        {
+            var inputDay1 = File.ReadAllLines("Input/input_Day1.txt").ToList();
+            
+
+            var d1p1 = Day1.Day1_p1(inputDay1);
+            var d1p2 = Day1.Day1_p2(inputDay1);
+
+            Console.WriteLine(string.Format("Day1Part1: {0}", d1p1));
+            Console.WriteLine(string.Format("Day1Part2: {0}", d1p2));
+
+            Console.Read();
+        }
+    }
+}
---
+++ AdventOfCode2022_Csharp/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode2022_Csharp
+{
+    public static class Day1
+    {
+        public  static int Day1_p1(List<String> inputDay1)
+        {
+
+            var lstSum = getSumsList(inputDay1);
+            return lstSum.Max();
+            
+        }
+
+        public static int Day1_p2(List<String> inputDay1)
+        {
+
+            var lstSum = getSumsList(inputDay1);
+
+            var pippo = lstSum.OrderByDescending(x => x).Take(3).Count();
+            return lstSum.OrderByDescending(x => x).Take(3).Sum();
+
+
+        }
+
+        public static List<int> getSumsList(List<String> inputDay1)
+        {
+            var lstSum = new List<int>();
+            int sum = 0;
+            for (int i = 0; i < inputDay1.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(inputDay1[i]))
+                {
+                    sum += Convert.ToInt32(inputDay1[i]);
+                }
+                else
+                {
+                    lstSum.Add(sum);
+                    sum = 0;
+                }
+            }
+
+            return lstSum;
+        }
+    }
+}
