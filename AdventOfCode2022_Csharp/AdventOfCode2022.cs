bb29801068c0d9c193493206f2333306c6073106
Lorenzo Curzi <l.curzi@sogeitsolutions.com>
2022-12-04 10:11:03 +0100

    Day 4

--- AdventOfCode2022_Csharp/AdventOfCode2022.cs
+++ AdventOfCode2022_Csharp/AdventOfCode2022.cs
@@ -11,6 +11,7 @@ namespace AdventOfCode2022_Csharp
             var inputDay1 = File.ReadAllLines("Input/input_Day1.txt").ToList();
             var inputDay2 = File.ReadAllLines("Input/input_Day2.txt").ToList();
             var inputDay3 = File.ReadAllLines("Input/input_Day3.txt").ToList();
+            var inputDay4 = File.ReadAllLines("Input/input_Day4.txt").ToList();
 
             var d1p1 = Day1.Day1_p1(inputDay1);
             var d1p2 = Day1.Day1_p2(inputDay1);
@@ -18,7 +19,9 @@ namespace AdventOfCode2022_Csharp
             var d2p2 = Day2.Day2_p2(inputDay2);
             var d3p1 = Day3.Day3_p1(inputDay3);
             var d3p2 = Day3.Day3_p2(inputDay3);
-           
+            var d4p1 = Day4.Day4_p1(inputDay4);
+            var d4p2 = Day4.Day4_p2(inputDay4);
+            Day4.RunTestsDay4();
 
             Console.WriteLine(string.Format("Day1Part1: {0}", d1p1));
             Console.WriteLine(string.Format("Day1Part2: {0}", d1p2));
@@ -26,7 +29,9 @@ namespace AdventOfCode2022_Csharp
             Console.WriteLine(string.Format("Day2Part2: {0}", d2p2));
             Console.WriteLine(string.Format("Day3Part1: {0}", d3p1));
             Console.WriteLine(string.Format("Day3Part2: {0}", d3p2));
-            
+            Console.WriteLine(string.Format("Day4Part1: {0}", d4p1));
+            Console.WriteLine(string.Format("Day4Part2: {0}", d4p2));
+
 
             Console.Read();
         }
---
+++ AdventOfCode2022_Csharp/Day4/Day4.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using AdventOfCode2022_Csharp.Utilities;
+
+namespace AdventOfCode2022_Csharp
+{
+    public static class Day4
+    {
+        public static int Day4_p1(List<String> inputDay4)
+        {
+            int sum = 0;
+            int seq1_start, seq1_end, seq2_start, seq2_end;
+
+            inputDay4.ForEach(x => {
+                seq1_start = Convert.ToInt32(x.Split(",")[0].ToString().Split("-")[0]);
+                seq1_end = Convert.ToInt32(x.Split(",")[0].ToString().Split("-")[1]);
+                seq2_start = Convert.ToInt32(x.Split(",")[1].ToString().Split("-")[0]);
+                seq2_end = Convert.ToInt32(x.Split(",")[1].ToString().Split("-")[1]);
+
+                if (
+                    (seq2_start <= seq1_start && seq1_start <= seq2_end && seq2_start <= seq1_end && seq1_end <= seq2_end) ||
+                    (seq1_start <= seq2_start && seq2_start <= seq1_end && seq1_start <= seq2_end && seq2_end <= seq1_end)
+                )
+                {
+                    sum++;
+                }
+
+            });
+
+            return sum;
+
+        }
+        public static int Day4_p2(List<String> inputDay4)
+        {
+            int sum = 0;
+            int seq1_start, seq1_end, seq2_start, seq2_end;
+
+            inputDay4.ForEach(x => {
+                seq1_start = Convert.ToInt32(x.Split(",")[0].ToString().Split("-")[0]);
+                seq1_end = Convert.ToInt32(x.Split(",")[0].ToString().Split("-")[1]);
+                seq2_start = Convert.ToInt32(x.Split(",")[1].ToString().Split("-")[0]);
+                seq2_end = Convert.ToInt32(x.Split(",")[1].ToString().Split("-")[1]);
+
+                if (
+                    ((seq2_start <= seq1_start && seq1_start <= seq2_end) || (seq2_start <= seq1_end && seq1_end <= seq2_end)) ||
+                    ((seq1_start <= seq2_start && seq2_start <= seq1_end) || (seq1_start <= seq2_end && seq2_end <= seq1_end))
+                )
+                {
+                    sum++;
+                }
+
+            });
+
+            return sum;
+
+        }
+
+        public static void RunTestsDay4()
+        {
+            Day4_Part1_Test();
+            Day4_Part2_Test();
+
+        }
+        public static void Day4_Part1_Test()
+        {
+
+            var tests = new List<Tuple<List<string>, int>>
+            {
+                new Tuple<List<string>, int>(new List<string>() {
+                    "2-4,6-8",
+                    "2-3,4-5",
+                    "5-7,7-9",
+                    "2-8,3-7",
+                    "6-6,4-6",
+                    "2-6,4-8",
+                }, 2),
+            };
+
+            tests.ForEach(test => Helpers.AssertEqual(Day4_p1(test.Item1), test.Item2, true));
+        }
+        public static void Day4_Part2_Test()
+        {
+
+            var tests = new List<Tuple<List<string>, int>>
+            {
+                new Tuple<List<string>, int>(new List<string>() {
+                    "2-4,6-8",
+                    "2-3,4-5",
+                    "5-7,7-9",
+                    "2-8,3-7",
+                    "6-6,4-6",
+                    "2-6,4-8",
+                }, 4),
+            };
+
+            tests.ForEach(test => Helpers.AssertEqual(Day4_p2(test.Item1), test.Item2, true));
+        }
+    }
+}
